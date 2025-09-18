19b4d7799e18c844de27a1594ecb4d0ffeeab587
Roger-Oxlaj <roxlaje@miumg.edu.gt>
2025-09-18 16:20:32 -0600

    Add files via upload

---
+++ middleware.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const token = req.cookies.get("token")?.value;
+  const { pathname } = req.nextUrl;
+
+  const rutasPublicas = ["/"];
+
+  if (!token && !rutasPublicas.includes(pathname)) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (token && pathname === "/") {
+    return NextResponse.redirect(new URL("/mapa", req.url));
+  }
+
+  return NextResponse.next();
+}
+export const config = {
+  matcher: ["/((?!_next|.*\\..*).*)"],
+};
