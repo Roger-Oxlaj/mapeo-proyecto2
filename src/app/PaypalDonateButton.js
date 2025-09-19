ca6d1e89e5ced3e5c53c5b511b849ad3563a6b0e
Roger Oxlaj <roxlaje@umg.edu.gt>
2025-09-19 14:35:46 -0600

    actualizado

--- src/app/PaypalDonateButton.js
+++ src/app/PaypalDonateButton.js
@@ -30,37 +30,3 @@ export default function PaypalDonateButton() {
     </form>
   );
 }
-
-export default function PaypalDonateButton() {
-  return (
-    <form
-      style={{ textAlign: "center" }}
-      action="https://www.paypal.com/donate"
-      method="post"
-      target="_blank" // <-- abre en otra pestaña
-      rel="noopener noreferrer" // <-- buena práctica por seguridad
-    >
-      <p style={{ fontSize: "24px", fontWeight: "bold", fontStyle: "italic" }}>
-        Apoya al creador
-      </p>
-      <input type="hidden" name="hosted_button_id" value="E8NWCWP449YDS" />
-      <input
-        type="image"
-        src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-        style={{ width: "200px", height: "auto" }}
-        border="0"
-        name="submit"
-        title="PayPal - The safer, easier way to pay online!"
-        alt="Donate with PayPal button"
-      />
-      <img
-        alt=""
-        border="0"
-        src="https://www.paypal.com/en_GT/i/scr/pixel.gif"
-        width="1"
-        height="1"
-      />
-    </form>
-  );
-}
-
